c708361017bb269cabc9ce36640f8a9e12ef7d36
zivraf <zivraf@hotmail.com>
2019-12-04 09:47:51 -0800

    update location

--- get-app.sh
+++ get-app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 size=""
 ulimit -n 20000
-/usr/bin/wget -O /opt/server https://github.com/vmssignite/ignite2019/raw/controlcrash/server
+/usr/bin/wget -O /opt/server https://github.com/zivraf/E2EDemo/raw/master/server 
 sleep 5
 chmod +x /opt/server
 /opt/server
